66db0294bb0073207f6e6308ba9b7dcb912294d8
Luke De Feo <luke.defeo@just-eat.com>
2019-06-17 20:56:23 +0100

    added shortcut for clean context kill

--- src/background/background.ts
+++ src/background/background.ts
@@ -40,6 +40,16 @@ function windowIdContextIdMapping(): WindowContextMapping {
 
 //maybe window state could have a pointer to the actual context... the context can change independantly of this data, its better to think of them relationally
 
+chrome.commands.onCommand.addListener(async (command: string) => {
+
+  console.log(`got command ${command}`)
+  if (command === 'clean_context_kill') {
+    const window = await chrome.windows.getCurrent()
+    await cleanContextKill(window.id)
+  }
+})
+
+
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
 
   const msgType: MessageRequestType = message.type
@@ -146,73 +156,3 @@ async function moveTabToContext(tab: Tab, contextId: number) {
     })
   }
 }
-
-/*
-
-   defn window-id->context-id [window-id]
- (get (set/map-invert @*context->window-state) window-id))
-
- (defn handle-window-focused! [window-id]
- (swap! *ordered-windows-state #(distinct (cons window-id %)))
- (println @*ordered-windows-state "<<<  windows focused"))
-
- (defn handle-closed-window! [window-id]
- (swap! *ordered-windows-state #(remove (partial = window-id) %))
- (if-let [context-id (window-id->context-id window-id)]
- (do
- (swap! *context->window-state dissoc context-id)
- (println "removed window " window-id " from state for context" context-id))
- (println "unknown window closed"))
- (println "state now" @*context->window-state))
-
- (defn join-window-id-to-contexts [contexts window-state ordered-windows]
- (let [inverted-window-state (set/map-invert window-state)
-
- order-windows-with-ctx-id (map (fn [window-id]
- {:id        (get inverted-window-state window-id :unmanaged)
-                                          :window-id window-id}) (remove #(= % -1) ordered-windows))]
- (map (fn [{:keys [id window-id]}]
-
- (let [name (->>
- contexts
- (filter (fn [x] (= id (:id x))))
- first
- :name)]
- {:window-id window-id
-              :name      (or name "Unmanaged")}))
- order-windows-with-ctx-id)))
-
- (defn get-context-switcher-state []
- (join-window-id-to-contexts @*contexts @*context->window-state @*ordered-windows-state))
-
- ;;todo migrate to this in util
- (defn find-context-by-id [id contexts]
- (select-first [ALL (selected? :id #(= % id))] @*contexts))
-
- (defn <clean-current-context []
- (go
- (let [{:keys [id tabs] :as current-window} (js->clj-keyed-first (<! (windows/get-current (clj->js {:populate true}))))
-
- current-ctx-id (window-id->context-id id)
-
- current-ctx (select-first [ALL (selected? :id #(= % current-ctx-id))] @*contexts)
-
- to-kill (->>
- tabs
- (remove (fn [tab] (url-matches-context? (:url tab) current-ctx)))
- (map :id))]
-
- (when current-ctx
- (tabs/remove (clj->js to-kill))))))
- /**
- (defn process-chrome-event [event]
- (go
- (let [[event-id event-args] event]
- (case event-id
- ::runtime/on-connect (apply handle-client-connection! event-args)
- ::windows/on-removed (tm/handle-closed-window! (first event-args))
- ::windows/on-focus-changed (tm/handle-window-focused! (first (js->clj-keyed event-args)))
- ::tabs/on-updated (<! (tm/process-updated-tab! event-args))
- ::commands/on-command (<! (tm/<clean-current-context))
- nil))))
- */
